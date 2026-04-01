acbd04372f40e92104b364010b110ae4d89a896e
Benjamin Wang <benjamin.ahrtr@gmail.com>
2026-04-01 20:48:44 +0100

    Use GetAutoLeave to read AutoLeave, and new(boolVar) to set value
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- util.go
+++ util.go
@@ -98,7 +98,7 @@ func DescribeConfState(state *pb.ConfState) string {
 	}
 	return fmt.Sprintf(
 		"Voters:%v VotersOutgoing:%v Learners:%v LearnersNext:%v AutoLeave:%v",
-		state.Voters, state.VotersOutgoing, state.Learners, state.LearnersNext, state.AutoLeave,
+		state.Voters, state.VotersOutgoing, state.Learners, state.LearnersNext, state.GetAutoLeave(),
 	)
 }
 
