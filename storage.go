345439b79abe9d94d0d5df44027ce374178c2c8e
Benjamin Wang <benjamin.ahrtr@gmail.com>
2026-04-01 21:18:10 +0100

    Fix test failures
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- storage.go
+++ storage.go
@@ -126,6 +126,7 @@ func NewMemoryStorage() *MemoryStorage {
 func (ms *MemoryStorage) InitialState() (pb.HardState, pb.ConfState, error) {
 	ms.callStats.initialState++
 	cs := ms.snapshot.GetMetadata().GetConfState()
+	cs = pb.EnsureConfState(cs)
 	return ms.hardState, *cs, nil
 }
 
