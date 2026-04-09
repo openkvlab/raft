db76f731fb82eca9197b1f695f3b94c55ed0bc42
Benjamin Wang <benjamin.ahrtr@gmail.com>
2026-04-09 21:59:32 +0100

    Cleanup unused EntrySliceToPointers and EntrySliceFromPointers
    Signed-off-by: Benjamin Wang <benjamin.ahrtr@gmail.com>

--- raftpb/util.go
+++ raftpb/util.go
@@ -45,31 +45,3 @@ func EnsureSnapshot(s *Snapshot) *Snapshot {
 	s.Metadata = EnsureSnapshotMetadata(s.Metadata)
 	return s
 }
-
-// EntrySliceToPointers converts a slice of Entry values to a slice of Entry pointers.
-// TODO: remove this function
-func EntrySliceToPointers(ents []Entry) []*Entry {
-	if ents == nil {
-		return nil
-	}
-	result := make([]*Entry, len(ents))
-	for i := range ents {
-		result[i] = &ents[i]
-	}
-	return result
-}
-
-// EntrySliceFromPointers converts a slice of Entry pointers to a slice of Entry values.
-// TODO: remove this function
-func EntrySliceFromPointers(ents []*Entry) []Entry {
-	if ents == nil {
-		return nil
-	}
-	result := make([]Entry, len(ents))
-	for i := range ents {
-		if ents[i] != nil {
-			result[i] = *ents[i]
-		}
-	}
-	return result
-}
